3686b4bda783bb1a1da049de200a1307b2bb896e
Ruby4President <m.jibril1008@gmail.com>
2025-04-22 10:57:06 +0700

    Add files via upload

---
+++ db_pengguna/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head><title>Login - SMK Bunda Kandung</title></head>
+        <body>
+            <h2 align="center">Form Login</h2>
+            <form action="login.php" method="post" align="center">
+                <input type="text" name="username" placeholder="Username" required><br><br>
+                <input type="password" name="password" placeholder="Password" required><br><br>
+                <input type="submit" value="Login">
+            </form>
+        </body>
+</html>
